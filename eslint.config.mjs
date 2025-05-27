82e361549a3dc74cf20324529ddfa97c72f4ca1c
Luciano Aedo Puga <81434301+shvmeless@users.noreply.github.com>
2025-05-27 11:47:42 -0400

    chore: update `eslint` version and config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,20 @@
+// IMPORTS
 import globals from 'globals'
 import javascript from '@eslint/js'
 import typescript from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
 import love from 'eslint-config-love'
 import stylistic from '@stylistic/eslint-plugin'
 
+// RULES
 const rules = {
   '@typescript-eslint/array-type': ['error', { default: 'generic', readonly: 'generic' }],
   '@typescript-eslint/no-dynamic-delete': 'off',
   '@typescript-eslint/no-magic-numbers': 'off',
 
-  "@typescript-eslint/prefer-destructuring": [ 'error', { "array": true, "object": true },
+  "@typescript-eslint/prefer-destructuring": [
+    'error',
+    { "array": true, "object": true },
     {
       "enforceForRenamedProperties": false,
       "enforceForDeclarationWithTypeAnnotation": false
@@ -31,14 +36,21 @@ const rules = {
   '@stylistic/arrow-parens': ['error', 'always'],
 }
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { languageOptions: { globals: globals.node } },
+// CONFIG
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    plugins: { js: javascript }
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: {
+      globals: globals.node
+    }
+  },
   javascript.configs.recommended,
-  ...typescript.configs.recommended,
+  typescript.configs.recommended,
   { ...love },
-  stylistic.configs['recommended-flat'],
+  stylistic.configs['recommended'],
   { rules }
-]
+])
